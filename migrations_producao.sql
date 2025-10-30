8318073b7cc0c12899a7ff0fb45e408e4e68552f
Gabriel Sanz <gabriel.sanz@infortechms.com.br>
2025-10-30 16:47:13 -0400

    chore: remover migration vazia NotificationPreferences e gerar script SQL para deploy

---
+++ migrations_producao.sql
@@ -0,0 +1,45 @@
+﻿START TRANSACTION;
+
+ALTER TABLE users ADD password_reset_token text;
+
+ALTER TABLE users ADD password_reset_token_expiry timestamp with time zone;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20251030193120_AdicionarTokenRecuperacaoSenha', '8.0.11');
+
+COMMIT;
+
+START TRANSACTION;
+
+ALTER TABLE companies ADD address_city text;
+
+ALTER TABLE companies ADD address_complement text;
+
+ALTER TABLE companies ADD address_country text;
+
+ALTER TABLE companies ADD address_neighborhood text;
+
+ALTER TABLE companies ADD address_number text;
+
+ALTER TABLE companies ADD address_state text;
+
+ALTER TABLE companies ADD address_street text;
+
+ALTER TABLE companies ADD address_zip_code text;
+
+ALTER TABLE companies ADD phone_landline text;
+
+ALTER TABLE companies ADD phone_mobile text;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20251030193745_AdicionarContatoEnderecoEmpresa', '8.0.11');
+
+COMMIT;
+
+START TRANSACTION;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20251030200333_CriarTabelaNotificationPreferences', '8.0.11');
+
+COMMIT;
+
