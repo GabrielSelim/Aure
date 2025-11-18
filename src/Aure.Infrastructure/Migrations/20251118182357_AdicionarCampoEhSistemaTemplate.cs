c10229a5351759724c64656a8ce1748c6c8fc3e4
Gabriel Sanz <gabriel.sanz@infortechms.com.br>
2025-11-18 14:24:32 -0400

    feat: adicionar campo EhSistema para proteger templates do sistema

---
+++ src/Aure.Infrastructure/Migrations/20251118182357_AdicionarCampoEhSistemaTemplate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aure.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarCampoEhSistemaTemplate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "eh_sistema",
+                table: "contracttemplates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "eh_sistema",
+                table: "contracttemplates");
+        }
+    }
+}
